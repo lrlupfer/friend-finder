010b2e779f54a9071131b9510c54d7d6376fb5e4
lindsey <lrlupfer@gmail.com>
2017-04-27 10:46:48 -0400

    demo fix for deployment issues

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var path = require("path");
+var cool = require('cool-ascii-faces');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -16,6 +17,9 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 require(`./app/routing/html-routes`)(app);
 require(`./app/routing/api-routes`)(app);
 
+app.get('/cool', function(request, response) {
+  response.send(cool());
+});
 
 app.listen(PORT, function() {
   console.log("App listening on PORT " + PORT);
